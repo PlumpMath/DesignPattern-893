08ba8acf93ec803a4de2c7dff2c6e4f9dce6f024
HIDARI <ika5140@gmail.com>
2013-05-02 12:47:05 +0900

    add code use IFileIO and FileProperties in Main.

--- DesignPattern/DesignPattern.cs
+++ DesignPattern/DesignPattern.cs
@@ -17,6 +17,18 @@ namespace DesignPattern
 			IPrint printer = new PrintBsnner ("Hello");
 			printer.PrintWeak ();
 			printer.PrintStrong ();
+
+			IFileIO fileo = new FileProperties ();
+			fileo.SetValue ("Year", "2013");
+			fileo.SetValue ("Month", "05");
+			fileo.SetValue ("Day", "02");
+			fileo.WriteToFile ("FileIOExample.txt");
+
+			IFileIO filei = new FileProperties ();
+			filei.ReadFromFile("FileIOExample.txt");
+			Console.WriteLine (filei.GetValue ("Year"));
+			Console.WriteLine (filei.GetValue ("Month"));
+			Console.WriteLine (filei.GetValue ("Day"));
 		}
 	}
 }
