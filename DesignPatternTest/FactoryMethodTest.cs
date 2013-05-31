770af671b6b28e931936a681f9596a5c6a5b63d6
HiDARi <ika5140@gmail.com>
2013-05-31 12:49:32 +0900

    add FactoryMethod TestCase without assertion.

--- DesignPatternTest/FactoryMethodTest.cs
+++ DesignPatternTest/FactoryMethodTest.cs
@@ -1,14 +1,20 @@
 using System;
 using NUnit.Framework;
-using DesignPattern.FactoryMethod;
+using DesignPattern.FactoryMethod.Framework;
+using DesignPattern.FactoryMethod.IdCard;
 
 namespace DesignPatternTest
 {
+	[TestFixture]
 	public class FactoryMethodTest
 	{
-		public FactoryMethodTest ()
+		[TestCase("John Doe")]
+		[TestCase("HiDARi")]
+		public void CreateIDCardThenUse (string name)
 		{
-
+			var factory = new IDCardFactory ();
+			var id = factory.Create (name);
+			id.Use ();
 		}
 	}
 }
