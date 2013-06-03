11907f46ba2b8dc73baa8fa7f8cfea7f1c0e2039
HiDARi <ika5140@gmail.com>
2013-06-03 22:54:00 +0900

    3回GetInstaceを呼ぶテストを追加

--- DesignPatternTest/SingletonTest.cs
+++ DesignPatternTest/SingletonTest.cs
@@ -16,6 +16,17 @@ namespace DesignPatternTest
 			Assert.That (obj1, Is.EqualTo (obj2));
 			Console.WriteLine ("End.");
 		}
+		[TestCase]
+		public void CallGetInstanceThreeTimes()
+		{
+			Console.WriteLine ("Start.");
+			var obj1 = Singleton.GetInstance ();
+			var obj2 = Singleton.GetInstance ();
+			var obj3 = Singleton.GetInstance ();
+			Assert.That (obj1, Is.EqualTo (obj2));
+			Assert.That (obj1, Is.EqualTo (obj3));
+			Console.WriteLine ("End.");
+		}
 	}
 }
 
