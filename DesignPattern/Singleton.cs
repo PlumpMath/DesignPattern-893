318b28d42e96fdd173056abd4b3c9549ff6aa8af
HiDARi <ika5140@gmail.com>
2013-06-03 22:31:50 +0900

    Singletonクラスを実装

--- DesignPattern/Singleton.cs
+++ DesignPattern/Singleton.cs
@@ -1,11 +1,21 @@
 using System;
 
-namespace DesignPattern
+namespace DesignPattern.Singleton
 {
 	public class Singleton
 	{
+		// In C#, When create instance ?
+
+		private static Singleton singleton = new Singleton ();
+
 		public Singleton ()
 		{
+			Console.WriteLine ("¥n****** Singleton Initialized *****¥n");
+		}
+
+		public static Singleton GetInstance ()
+		{
+			return singleton;
 		}
 	}
 }
