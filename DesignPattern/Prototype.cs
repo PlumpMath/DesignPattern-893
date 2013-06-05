5733500ccd276e4e8818bd7a3e405c675f94e0bc
HiDARi <ika5140@gmail.com>
2013-06-05 20:20:00 +0900

    Prototypeパターンのサンプルを実装開始．
    プロダクトコード？とテストコードのファイルを追加．

---
+++ DesignPattern/Prototype.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace DesignPattern.Prototype
+{
+	namespace Framework
+	{
+		public interface IProduct
+		{
+
+		}
+
+		public class Manager
+		{
+			public Manager ()
+			{
+			}
+		}
+	}
+}
+
---
+++ DesignPatternTest/PrototypeTest.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DesignPatternTest
+{
+	public class PrototypeTest
+	{
+		public PrototypeTest ()
+		{
+		}
+	}
+}
+
