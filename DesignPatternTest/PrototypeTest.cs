59fe5b1036420424d762a1ef5905b4e3a62bded6
HiDARi <ika5140@gmail.com>
2013-06-05 22:59:40 +0900

    テストコードから書き始めた．

--- DesignPatternTest/PrototypeTest.cs
+++ DesignPatternTest/PrototypeTest.cs
@@ -1,11 +1,18 @@
 using System;
+using NUnit.Framework;
+using DesignPattern.Prototype.Framework;
 
 namespace DesignPatternTest
 {
+	[TestFixture]
 	public class PrototypeTest
 	{
-		public PrototypeTest ()
+		[TestCase]
+		public void UsePrototypeWithUnderLinePenTest ()
 		{
+			Manager manager = new Manager ();
+			UnderLinePen upen = new UnderLinePen ('~');
+			manager.register ("strong message", upen);
 		}
 	}
 }
