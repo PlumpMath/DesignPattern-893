a31c7616997b4635692b3d54f1f4600a72d634c4
HiDARi <ika5140@gmail.com>
2013-06-10 23:00:36 +0900

    Directorクラスの途中まで書いた

--- DesignPattern/Builder.cs
+++ DesignPattern/Builder.cs
@@ -2,10 +2,20 @@ using System;
 
 namespace DesignPattern.Builder
 {
-	public class Hoge
+	public abstract class Builder
 	{
-		public Hoge ()
+		public abstract void MakeTitle(string title);
+		public abstract void MakeString (string str);
+		public abstract void MakeItems (string[] items);
+		public abstract void Close ();
+	}
+
+	public class Director
+	{
+		private Builder builder;
+		public Director (Builder builder)
 		{
+			this.builder = builder;
 		}
 	}
 }
