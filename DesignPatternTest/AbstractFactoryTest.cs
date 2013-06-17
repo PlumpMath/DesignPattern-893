7f2f5a52465a6946ac357bb6ebbede7fb49784cf
HiDARi <ika5140@gmail.com>
2013-06-18 08:50:36 +0900

    引き続きAFのテストフィクスチャを実装中

--- DesignPatternTest/AbstractFactoryTest.cs
+++ DesignPatternTest/AbstractFactoryTest.cs
@@ -1,5 +1,5 @@
 using System;
-using DesignPattern.AbstractFactory;
+using DesignPattern.AbstractFactory.Factory;
 using NUnit.Framework;
 
 namespace DesignPatternTest
@@ -11,6 +11,18 @@ namespace DesignPatternTest
 		public void UseListFactoryTest ()
 		{
 			Factory factory = Factory.GetFactory ("hoge");
+			Link asahi = factory.CreateLink("AsahiShinbun", "http://www.asahi.com/");
+			Link yomiuri = factory.CreateLink ("Yomiuri", ",http://www.yomiuri.co.jp/");
+			Link us_Yahoo = factory.CreateLink("Yahoo!","http://www.yahoo.com/");
+			Link jp_Yahoo = factory.CreateLink ("Yahoo!Japan", "http://www yahoo・co.jp/");
+			Link excite = factory.CreateLink("Excite", "http://www.excite.com/");
+			Link google = factory.CreateLink ("Google", "http://www.google.com/");
+
+			Tray newsTray = factory.CreateTray ("NewsPaper");
+			newsTray.Add (asahi);
+			newsTray.Add (yomiuri);
+
+			Tray yahooTray = factory.CreateTray ("Yahoo!");
 
 		}
 	}
