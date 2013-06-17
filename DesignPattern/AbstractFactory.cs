48afec17247616df503b7209e7c6a773a3e77b06
HiDARi <ika5140@gmail.com>
2013-06-17 12:48:55 +0900

    抽象クラスItem，Link，Trayを追加

--- DesignPattern/AbstractFactory.cs
+++ DesignPattern/AbstractFactory.cs
@@ -1,11 +1,42 @@
 using System;
+using System.Collections.Generic;
 
 namespace DesignPattern.AbstractFactory
 {
-	public class AbstractFactory
+	namespace Factory
 	{
-		public AbstractFactory ()
+		public abstract class Item
 		{
+			protected string caption;
+			public Item (string caption)
+			{
+				this.caption = caption;
+			}
+
+			// return HTML string.
+			public abstract string MakeHtml();
+		}
+
+		public abstract class Link:Item
+		{
+			protected string url;
+			public Link(string caption, string url)
+				: base(caption)
+			{
+				this.url = url;
+			}
+		}
+
+		public abstract class Tray : Item
+		{
+			protected List<Item> tray = new List<Item> ();
+			public Tray (string caption)
+				:base(caption){}
+
+			public void Add(Item item)
+			{
+				tray.Add (item);
+			}
 		}
 	}
 }
